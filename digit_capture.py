2a94e66c146b325181ee0c49c61037ee4d0cd129
petesteamster <petesteamster@gmail.com>
2019-11-23 17:09:26 -0500

    added ability to display the counts by class
    added ability to display the counts by class. Added clear, erase, redraw grid buttons

--- digit_capture.py
+++ digit_capture.py
@@ -1,10 +1,112 @@
+def insert_mods_to_js_file(t_js_file,the_dict):
+    from digit_capture import set_file_name_prefix
+    from digit_capture import read_pickle,write_pickle
+    # this will read in a js file. Inputs will be
+    # the file name.
+    # It will retrieve the .pk file
+    # that has the dictionary that holds the mods to
+    # be inserted. It will return the file with the replaced
+    # values. This function enables rudumentry communication
+    # from python to javascript
+    fname_pfx=set_file_name_prefix(0)[0]
+    #pk_file=t_js_file[0,-3]+'.pk'
+    #the_dict=read_pickle(fname_pfx+pk_file)
+    #file1 = open(fname_pfx+t_js_file,"r+")
+    file1 = open(t_js_file,"r+")  
+    the_js_data=file1.read()
+    for old,new in the_dict.items():
+        the_js_data=the_js_data.replace(old,str(new))
+    return the_js_data
+#insert_mods_to_js_file(t_js_file,t_dict)
+def cross_ref_replace_dict(t_file_name):
+    import pandas as pd
+    from digit_capture import set_file_name_prefix
+    from digit_capture import get_global_settings as gs
+    t_full_side_size=gs('grid_count')*gs('pxl')
+    cross_ref_dict={'?width?':t_full_side_size,'?height?':t_full_side_size}
+    test_keys = ["?class00?",
+                 "?class01?",
+                 "?class02?",
+                 "?class03?",
+                 "?class04?",
+                 "?class05?",
+                 "?class06?",
+                 "?class07?",
+                 "?class08?",
+                 "?class09?",
+                 "?width?",
+                 "?height?"]
+    test_values = [0,0,0,0,0,0,0,0,0,0,t_full_side_size,t_full_side_size] 
+    # using dictionary comprehension 
+    # to convert lists to dictionary 
+    res = {test_keys[i]: test_values[i] for i in range(len(test_keys))} 
+    fname_pfx=set_file_name_prefix(0)[0]
+    df=pd.read_csv(fname_pfx+t_file_name,header=None)
+    t_df=df.iloc[:,0:2]
+    t_list=['class','replace_dict']
+    t_df.columns = t_list
+    dd=t_df.groupby('class').count().to_dict()
+    #print(dd['replace_dict'])
+    for k, v in dd['replace_dict'].items():
+        #print(k,dd['replace_dict'][k])
+        res[k]=dd['replace_dict'][k]
+    res['?grid_count?']=get_dict()['grid_count']
+    res['?pxl?']=get_dict()['pxl']
+    res['?line_wd?']=get_dict()['line_wd']
+    return res
+def get_dict():
+    #dictionary holds the values of global variables.
+    gbl={'grid_count':5,
+         'line_wd':5,
+         'pxl':50,
+         'd_2':50,
+         'final_side_size':28,
+         't_class':'?class02?'
+        }
+    return gbl
+def get_global_settings(t_var):
+        #used to look up the values of global variables 
+    gbl=get_dict()
+    rtn=gbl[t_var]
+    return rtn
+def mount_drive():
+    if(not in_colab()):
+        return
+    from google.colab import drive
+    drive.mount('/content/drive')
+def in_colab():
+    import sys
+    IN_COLAB = 'google.colab' in sys.modules
+    return IN_COLAB
+def set_file_name_prefix(colab):
+    # if colab=1, set up structure for colab.
+    # if colab=0, set up structure for local.
+    # if colab=2, set up structure when in /content/drive  
+    import sys
+    import os
+    t_prefix=""
+    t_path=""
+    if(in_colab()):
+        colab=1
+    if(colab==1):
+        t_path='/content/drive/My Drive/Colab Notebooks/digit_data_entry/digit_data_entry'
+        t_prefix=t_path+"/"
+    else:
+        if(colab==0):
+            t_prefix="/content/drive"
+        st=os.getcwd()    
+        t_path=st+"/"+t_prefix
+        t_prefix=t_path+"/"
+    return (t_prefix,t_path)    
 def restartkernel():
     from IPython.display import display_html
     display_html("<script>Jupyter.notebook.kernel.restart()</script>",raw=True)
 def separate_digits(t_image,t_dim,grid_count):
     import numpy as np
     import cv2
-    global final_side_size 
+    from digit_capture import get_global_settings as gs
+    final_side_size=gs('final_side_size')
+    grid_count=gs('grid_count')
     row_count=grid_count*grid_count
     #print("separate "+str(t_image.shape))
     #print("row_count "+row_count)
@@ -28,39 +130,34 @@ def separate_digits(t_image,t_dim,grid_count):
         st_1=st_1+t_dim
         st_2=0
     return (im_list,im_reshaped)                
-def save1(t_canvas_image):
+def save(t_canvas_image):
     #function used to save imaages to a csc file.
     #It 
     
     import pandas as pd
     import cv2
     import numpy as np
-    #filename = "user_input.jpg"
-    #output_image.save(filename)
-    #read in image
-    global d_2
-    global master
-    global w
-    global grid_count
-    global df_class
-    global df_column_names
+    from digit_capture import get_global_settings as gs
+    d_2=gs('d_2')
+    grid_count=gs('grid_count')
+    t_dest=set_file_name_prefix(0)
+    fname_pfx=t_dest[0]
+    t_path=t_dest[1]
+    t_class=gs('t_class')
+    final_side_size=gs('final_side_size')
+    df_class=make_class_list(t_class,(grid_count*grid_count))
+    df_column_names=make_col_list((final_side_size*final_side_size))
     out_array = np.zeros(((grid_count*grid_count),(d_2*d_2)),dtype='uint8')
-    #t_image=img = cv2.imread('user_input.jpg',0)
     (t_digits,t_reformat_dg)=separate_digits(t_canvas_image,d_2,grid_count)
-    #print('save '+str(t_reformat_dg.shape))
     new_data=reformat_mnist(t_reformat_dg,0)
     df_dta=pd.DataFrame(data=new_data)
     df_dta=pd.concat([df_class,df_dta],axis=1)
-    write_digit_data(df_dta,'digit_data.csv')
+    write_digit_data(df_dta,fname_pfx+'digit_data.csv')
     for i in range(0,(grid_count*grid_count)):
         new_out=np.reshape(new_data[i,:],(28,28))
         t_ind=str(i).zfill(5)
-        cv2.imwrite('digit'+t_ind+'.jpg',new_out)
-    #new_data=reformat_mnist(t_reformat_dg,0)
+        cv2.imwrite(fname_pfx+'digit'+t_ind+'.jpg',new_out)
     print(len(t_digits))
-    ##master.quit()
-    #master.destroy()
-    #master.quit()
 def write_digit_data(t_data,file_name):
     import pandas as pd
     t_data.to_csv(file_name,header=False,mode='a',index=False)
@@ -90,23 +187,18 @@ def write_pickle(file_name,obj_name):
     #with open(file_name,'wb') as f:
     #    pk.dump(obj_name,f)
     pk.dump( obj_name, open( file_name, "wb" ) )
-def read_pickle(file_name,obj_name):
+def read_pickle(file_name):
     import pickle as pk
     rtn=0
     with open(file_name,'rb') as f:
-        rtn=pk.load(obj_name,f)
+        rtn=pk.load(f)
     return rtn    
 def getBestShift(img):
     import numpy as np
     import scipy.ndimage as ndi
     cy,cx = ndi.measurements.center_of_mass(255-img)
-    #print(str(cy)+" cy")
-    #print(str(cx)+ "cx")
-    #rows,cols = img.shape
     shiftx = np.round(28/2.0-cx).astype(int)
     shifty = np.round(28/2.0-cy).astype(int)
-    #print(str(shiftx)+ " shift x")
-    #print(str(shifty)+ " shift y")
     return shiftx,shifty
 
 def make_col_list(t_count):
@@ -127,7 +219,6 @@ def reshape_mnist(tData):
 def reformat_mnist(tData,t_invert):
     import numpy as np
     entries=tData.shape[0]
-    #np.zeros((28,28),dtype='int')
     new_format=np.zeros((entries,(28*28)),dtype='uint8')
     for i in range(0,entries):
         t_image=set_in_bounding_square(tData[i,:,:],20,i)
@@ -140,11 +231,9 @@ def load_and_threshold_file(t_file):
     import numpy as np
     gray1=cv2.imread(t_file,0)
     gray=cv2.imread(t_file,0)
-    print(type(gray))
     gray = cv2.resize(gray,(28, 28))
     (thresh, gray) = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     gray = 255-gray
-    #(thresh, gray) = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     gray=np.round((gray/255),decimals=0)
     gray=255*gray
     gray=np.uint8(gray)
@@ -154,9 +243,6 @@ def shift_2(img,t_invert):
     t_paste=np.zeros((28,28),dtype='int')
     r,c=img.shape
     x,y=getBestShift(img)
-    #print(str(r) + " " + 'r')
-    #print(str(c) + " " + 'c')
-    #print('shape img '+ str(img.shape)) 
     t_paste[y:(y+r),x:x+c]=img
     if(t_invert==1):
         t_paste=255-t_paste
@@ -166,16 +252,12 @@ def shift(img,sx,sy):
     import numpy as np
     rows,cols = img.shape
     M = np.float32([[1,0,sx],[0,1,sy]])
-    #print((M.shape))
-    #print((img.shape))
-    #shifted = cv2.warpAffine(img,M,(cols,rows))
-    #return shifted  
 def set_in_bounding_square(t_image,t_side,i):
     import numpy as np
     import cv2
     t_image=t_image.astype('uint8')
     t_image = cv2.resize(t_image,(28, 28))
-    print("bounding "+str(type(t_image[0,0]))+"   "+str(i))
+    #print("bounding "+str(type(t_image[0,0]))+"   "+str(i))
     #(thresh, t_image) = cv2.threshold(t_image, 20, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     #if(t_invert==0):        
     #    t_image = 255-t_image
@@ -207,4 +289,46 @@ def set_in_bounding_square(t_image,t_side,i):
     #print((t_image.shape))
     new_image=cv2.resize(t_image,(ncols,nrows))
     (thresh, new_image) = cv2.threshold(new_image, 20, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    return new_image    
+    return new_image
+def output_data(t_data):
+    from digit_capture import get_global_settings as gs
+    t_dest=set_file_name_prefix(0)
+    fname_pfx=t_dest[0]
+    dmy=len(t_data)
+    import numpy as np
+    import cv2
+    tAry=np.asarray(t_data)
+    #tAry
+    final_side_size=gs('final_side_size')
+    grid_count = gs('grid_count')
+    t_class=gs('t_class')
+    df_class=make_class_list(t_class,(grid_count*grid_count))
+    df_column_names=make_col_list((final_side_size*final_side_size))
+    pxl=gs('pxl')
+    line_wd=gs('line_wd')
+    canvas_width = grid_count*pxl
+    canvas_height = grid_count*pxl
+    prev_x=-999999;
+    prev_y=-999999;
+    x_coords=[]
+    y_coords=[]
+    width = canvas_width  # canvas width
+    height = canvas_height # canvas height
+    center = height//2
+    white = (255, 255, 255) # canvas back
+    t_bd1=5
+    t_bd2=175
+    d_1=1
+    d_2=gs('d_2')
+    d_3=120
+    d_4=180
+    wdh=1
+    #print(type(t_data))
+    tAry=np.asarray(t_data,dtype="uint8")
+    #tAry.shape
+    tAry2_dim0=int(len(tAry)/4)
+    tAry2=np.reshape(tAry,(tAry2_dim0,4))
+    nw_image=np.reshape(tAry2[0:tAry2_dim0,0],(canvas_width,canvas_width))
+    cv2.imwrite(fname_pfx+'python_version.jpg',nw_image)
+    print(type(nw_image[0,0]))
+    save(nw_image)
